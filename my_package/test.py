13b77bd97633bce30355e6f5fde58fe9852ca579
Nitin Verma <nitin.d.verma@pwc.com>
2016-04-25 11:41:43 +0000

    Initial commit.

---
+++ my_package/test.py
@@ -0,0 +1,4 @@
+
+
+def print_result():
+  print 'hello'
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='my_package',
+      version='0.1.0',
+      description='This is my first pip installable package.',
+      url='https://github.com/nitin478/first-pip-package',
+      author='Nitin Verma',
+      author_email='nitin478@gmail.com',
+      license='NA',
+      packages=['my_package'],
+      zip_safe=False)
